71416c691aa49366c1dd81dbbc8b444eceb1dfdc
채요원 <yowon0425@naver.com>
2025-01-22 09:23:19 +0900

    first-payment

--- intern/src/App.js
+++ intern/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProductList from './ProductList';
+import CardRegister from './CardRegister';
 
 function App() {
   return (
-    <div>
-      <ProductList />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/card-register" element={<CardRegister />} />
+      </Routes>
+    </Router>
   );
 }
 
